10ab71c9f3fe300d9b4edc04161139d013879110
leojunioyuri <leojunioyuri@hotmail.com>
2023-05-23 23:40:24 -0300

    feat: criando classe D

---
+++ ClassD.js
@@ -0,0 +1,37 @@
+class D {
+    constructor() {
+      this.D1 = ""; // atributo String D1
+      this.D2 = 0; // atributo int D2
+    }
+  
+    // Getter para D1
+    getD1() {
+      return this.D1;
+    }
+  
+    // Setter para D1
+    setD1(value) {
+      this.D1 = value;
+    }
+  
+    // Getter para D2
+    getD2() {
+      return this.D2;
+    }
+  
+    // Setter para D2
+    setD2(value) {
+      this.D2 = value;
+    }
+  
+    // Método MD1
+    MD1() {
+      console.log("Método MD1");
+    }
+  
+    // Método MD2
+    MD2() {
+      console.log("Método MD2");
+    }
+  }
+  
