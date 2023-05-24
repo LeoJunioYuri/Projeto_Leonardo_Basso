c59d8a98c0e1c022882e3724031255c218022c21
leojunioyuri <leojunioyuri@hotmail.com>
2023-05-23 23:47:35 -0300

    feat: criacao metodo md3

--- ClassD.js
+++ ClassD.js
@@ -1,37 +1,41 @@
 class D {
-    constructor() {
-      this.D1 = ""; // atributo String D1
-      this.D2 = 0; // atributo int D2
-    }
-  
-    // Getter para D1
-    getD1() {
-      return this.D1;
-    }
-  
-    // Setter para D1
-    setD1(value) {
-      this.D1 = value;
-    }
-  
-    // Getter para D2
-    getD2() {
-      return this.D2;
-    }
-  
-    // Setter para D2
-    setD2(value) {
-      this.D2 = value;
-    }
-  
-    // Método MD1
-    MD1() {
-      console.log("Método MD1");
-    }
-  
-    // Método MD2
-    MD2() {
-      console.log("Método MD2");
-    }
+  constructor() {
+    this.D1 = ""; // atributo String D1
+    this.D2 = 0; // atributo int D2
   }
-  
+
+  // Getter para D1
+  getD1() {
+    return this.D1;
+  }
+
+  // Setter para D1
+  setD1(value) {
+    this.D1 = value;
+  }
+
+  // Getter para D2
+  getD2() {
+    return this.D2;
+  }
+
+  // Setter para D2
+  setD2(value) {
+    this.D2 = value;
+  }
+
+  // Método MD1
+  MD1() {
+    console.log("Método MD1");
+  }
+
+  // Método MD2
+  MD2() {
+    console.log("Método MD2");
+  }
+
+  // Método MD3
+  MD3() {
+    console.log("Método MD3");
+  }
+}
